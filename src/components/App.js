6f992433080234aae290d8263c0442cabf4f51ba
TabithaKoech <tabbyk1718@gmail.com>
2022-07-21 11:01:25 +0300

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,26 +2,19 @@
 import React, { useEffect, useState } from 'react'
 
 function App() {
-    const [image,setImage] = useState([null])
-    if (!image) {
-
-    useEffect(() => {
-    fetch('https://dog.ceo/api/breeds/image/random')
-    .then(response => response.json())
-    .then(data => setImage(data.message))
+    const [images, setImages]=useState([]);
+    const [isloaded, setIsLoaded]=useState(false);
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response)=>response.json())
+        .then((data)=>{
+            setImages(data.message);
+            setIsLoaded(true);
+        })
     },[])
 
-        return <p>Loading...</p>
-    }
-   return (
-      <div>
-        <p>Here's a lovely fox for you:</p>
-        <img src={image} alt="A Random fox" />
-      </div>
-    );
-   
-
-  
+    if(!isloaded) return <p>Loading...</p>
+        return (<img src={images} alt="A Random Dog"></img>)
 }
 
 export default App
